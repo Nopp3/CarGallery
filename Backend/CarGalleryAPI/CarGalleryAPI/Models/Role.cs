d0b598d68abbbd65d2333feae4438b9dc08a0f55
Radek <radekrytelkuc@gmail.com>
2023-05-31 17:29:13 +0200

    Roles model changed to enum

--- Backend/CarGalleryAPI/CarGalleryAPI/Models/Role.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CarGalleryAPI.Models
-{
-    public class Role
-    {
-        public int id { get; set; }
-        public string name { get; set; }
-    }
-}
--- Backend/CarGalleryAPI/CarGalleryAPI/Models/User.cs
+++ Backend/CarGalleryAPI/CarGalleryAPI/Models/User.cs
@@ -1,10 +1,18 @@
-﻿namespace CarGalleryAPI.Models
+﻿
+namespace CarGalleryAPI.Models
 {
+    public enum Roles
+    {
+        Admin = 1,
+        User = 2
+    }
+
     public class User
     {
         public Guid id { get; set; }
         public int role_id { get; set; }
         public string username { get; set; }
+        public string? email { get; set; }
         public string password { get; set; }
     }
 }
