d5e8e7ca4b3a10119b3125ec74c1661166fea18b
Radek <radekrytelkuc@gmail.com>
2023-05-31 17:30:42 +0200

    Password encrypting function

---
+++ Backend/CarGalleryAPI/CarGalleryAPI/Data/Hash.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Intrinsics.Arm;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CarGalleryAPI.Data
+{
+    public static class Hash
+    {
+        public static string Encrypt(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                string hash = BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+                return hash;
+            }
+        }
+    }
+}
