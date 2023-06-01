2ff907573be0af7a789eb98ce736474bb6d5aa66
Radek <radekrytelkuc@gmail.com>
2023-06-01 16:51:05 +0200

    Bodies & Brands Controllers

---
+++ Backend/CarGalleryAPI/CarGalleryAPI/Controllers/BodiesController.cs
@@ -0,0 +1,34 @@
+﻿using CarGalleryAPI.Models;
+using CarGalleryAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarGalleryAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BodiesController : Controller
+    {
+        private readonly DatabaseContext _dbContext;
+        public BodiesController(DatabaseContext dbContext) => _dbContext = dbContext;
+
+        [HttpGet]
+        public async Task<IActionResult> GetBodies()
+        {
+            List<Body> Bodies = await _dbContext.Bodies.ToListAsync();
+            return Ok(Bodies);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddBody([FromBody] string type)
+        {
+            Body body = new Body();
+            body.type = type;
+
+            await _dbContext.Bodies.AddAsync(body);
+            await _dbContext.SaveChangesAsync();
+
+            return Ok(body);
+        }
+    }
+}
---
+++ Backend/CarGalleryAPI/CarGalleryAPI/Controllers/BrandsController.cs
@@ -0,0 +1,34 @@
+﻿using CarGalleryAPI.Models;
+using CarGalleryAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarGalleryAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BrandsController : Controller
+    {
+        private readonly DatabaseContext _dbContext;
+        public BrandsController(DatabaseContext dbContext) => _dbContext = dbContext;
+
+        [HttpGet]
+        public async Task<IActionResult> GetBrands()
+        {
+            List<Brand> Brands = await _dbContext.Brands.ToListAsync();
+            return Ok(Brands);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddBody([FromBody] string name)
+        {
+            Brand brand = new Brand();
+            brand.name = name;
+
+            await _dbContext.Brands.AddAsync(brand);
+            await _dbContext.SaveChangesAsync();
+
+            return Ok(brand);
+        }
+    }
+}
