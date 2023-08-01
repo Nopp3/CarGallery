f7dd86eb5e14f592fe846fc5c3fde5a00db1db9e
Radek <radekrytelkuc@gmail.com>
2023-08-01 15:29:13 +0200

    Added car images to home component

--- Frontend/CarGallery/src/app/components/home/home.component.ts
+++ Frontend/CarGallery/src/app/components/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { SessionService } from "../../services/session/session.service";
+import { Router } from "@angular/router";
+import { CarUI } from "../../models/car.model";
+import { CarService } from "../../services/car/car.service";
 
 @Component({
   selector: 'app-home',
@@ -6,5 +10,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  constructor(private router : Router, private carService: CarService) {}
+  cars: CarUI[] = []
+  ngOnInit(){
+    if (SessionService.get("ActiveUser") == null){
+      this.router.navigate(['login'])
+    }
+    this.carService.getUserCars()
+      .subscribe({
+        next: value => this.cars = value
+      })
+  }
 
+  deleteCar(id: string){
+    console.log(id)
+  }
 }
