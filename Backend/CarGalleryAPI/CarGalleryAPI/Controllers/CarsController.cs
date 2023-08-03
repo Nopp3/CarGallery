cb03006462299b68e26163d3236d0661842d35a2
Radek <radekrytelkuc@gmail.com>
2023-08-03 16:38:18 +0200

    Commented unnecessary function, Added function that returns only 1 car by id, Changed routing for GetUserCars

--- Backend/CarGalleryAPI/CarGalleryAPI/Controllers/CarsController.cs
+++ Backend/CarGalleryAPI/CarGalleryAPI/Controllers/CarsController.cs
@@ -28,7 +28,7 @@ namespace CarGalleryAPI.Controllers
                         select new
                         {
                             car.id,
-                            car.user_id,
+                            //car.user_id,
                             user.username,
                             fuel = fuel.type,
                             body = body.type,
@@ -45,6 +45,16 @@ namespace CarGalleryAPI.Controllers
         }
 
         [HttpGet]
+        public async Task<IActionResult> GetCar([FromQuery] Guid id)
+        {
+            Car car = await _dbContext.Cars.FindAsync(id);
+            if (car == null)
+                return NotFound("Not found car with id: " + id);
+            return Ok(car);
+        }
+
+        [HttpGet]
+        [Route("user")]
         public async Task<IActionResult> GetUserCars([FromQuery] Guid id)
         {
             List<Car> Cars = await _dbContext.Cars.ToListAsync();
@@ -63,7 +73,7 @@ namespace CarGalleryAPI.Controllers
                             select new
                             {
                                 car.id,
-                                car.user_id,
+                                //car.user_id,
                                 user.username,
                                 fuel = fuel.type,
                                 body = body.type,
@@ -81,44 +91,44 @@ namespace CarGalleryAPI.Controllers
             return NotFound();
         }
 
-        [HttpGet]
-        [Route("body")]
-        public async Task<IActionResult> GetCarsByBody([FromQuery] string body)
-        {
-            Console.WriteLine(body);
+        //[HttpGet]
+        //[Route("body")]
+        //public async Task<IActionResult> GetCarsByBody([FromQuery] string body)
+        //{
+        //    Console.WriteLine(body);
 
-            Body? bodies = await _dbContext.Bodies.FirstOrDefaultAsync(x => x.type == body);
-            List<Car> cars = await _dbContext.Cars.ToListAsync();
-            if (bodies != null && cars.Where(x => x.body_id == bodies.id).Any())
-            {
-                #region LINQ query
-                var query = from car in cars.Where(x => x.body_id == bodies.id)
-                            join user in _dbContext.Users on car.user_id equals user.id into userJoin
-                            from user in userJoin.DefaultIfEmpty()
-                            join fuel in _dbContext.Fuels on car.fuel_id equals fuel.id into fuelJoin
-                            from fuel in fuelJoin.DefaultIfEmpty()
-                            join brand in _dbContext.Brands on car.brand_id equals brand.id into brandJoin
-                            from brand in brandJoin.DefaultIfEmpty()
-                            select new
-                            {
-                                car.id,
-                                car.user_id,
-                                user.username,
-                                fuel = fuel.type,
-                                body = bodies.type,
-                                brand = brand.name,
-                                car.model,
-                                car.productionYear,
-                                car.engine,
-                                car.horsePower,
-                                car.imagePath
-                            };
-                #endregion
-                var result = query.ToList();
-                return Ok(result);
-            }
-            return NotFound();
-        }
+        //    Body? bodies = await _dbContext.Bodies.FirstOrDefaultAsync(x => x.type == body);
+        //    List<Car> cars = await _dbContext.Cars.ToListAsync();
+        //    if (bodies != null && cars.Where(x => x.body_id == bodies.id).Any())
+        //    {
+        //        #region LINQ query
+        //        var query = from car in cars.Where(x => x.body_id == bodies.id)
+        //                    join user in _dbContext.Users on car.user_id equals user.id into userJoin
+        //                    from user in userJoin.DefaultIfEmpty()
+        //                    join fuel in _dbContext.Fuels on car.fuel_id equals fuel.id into fuelJoin
+        //                    from fuel in fuelJoin.DefaultIfEmpty()
+        //                    join brand in _dbContext.Brands on car.brand_id equals brand.id into brandJoin
+        //                    from brand in brandJoin.DefaultIfEmpty()
+        //                    select new
+        //                    {
+        //                        car.id,
+        //                        //car.user_id,
+        //                        user.username,
+        //                        fuel = fuel.type,
+        //                        body = bodies.type,
+        //                        brand = brand.name,
+        //                        car.model,
+        //                        car.productionYear,
+        //                        car.engine,
+        //                        car.horsePower,
+        //                        car.imagePath
+        //                    };
+        //        #endregion
+        //        var result = query.ToList();
+        //        return Ok(result);
+        //    }
+        //    return NotFound();
+        //}
 
         [HttpGet]
         [Route("brand")]
@@ -139,7 +149,7 @@ namespace CarGalleryAPI.Controllers
                             select new
                             {
                                 car.id,
-                                car.user_id,
+                                //car.user_id,
                                 user.username,
                                 fuel = fuel.type,
                                 body = body.type,
