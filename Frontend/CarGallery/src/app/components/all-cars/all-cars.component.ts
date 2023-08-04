f7d9aa2f21aef20acdb0bed5ab42ac64ac5867bc
Radek <radekrytelkuc@gmail.com>
2023-08-04 13:01:47 +0200

    Added filters by brand in all cars component

--- Frontend/CarGallery/src/app/components/all-cars/all-cars.component.ts
+++ Frontend/CarGallery/src/app/components/all-cars/all-cars.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from "@angular/router";
 import { SessionService } from "../../services/session/session.service";
 import { CarService } from "../../services/car/car.service";
-import { CarUI } from "../../models/car.model";
+import { Brand, CarUI } from "../../models/car.model";
 
 @Component({
   selector: 'app-all-cars',
@@ -13,6 +13,8 @@ export class AllCarsComponent {
   constructor(private router: Router, private carService: CarService) {}
 
   cars: CarUI[] = []
+  brands: Brand[] = []
+  filter: number = 0
 
   ngOnInit(){
     if (SessionService.get("ActiveUser") == null){
@@ -22,5 +24,25 @@ export class AllCarsComponent {
       .subscribe({
         next: value => this.cars = value
       })
+    this.carService.getUsedBrands()
+      .subscribe({
+        next: value => this.brands = value
+      })
+  }
+  onFilterChange(){
+    switch (this.filter){
+      case 0:
+        this.carService.getAllCars()
+          .subscribe({
+            next: value => this.cars = value
+          })
+        break;
+      default:
+        this.carService.getCarsByBrand(this.filter)
+          .subscribe({
+            next: value => this.cars = value
+          })
+        break;
+    }
   }
 }
